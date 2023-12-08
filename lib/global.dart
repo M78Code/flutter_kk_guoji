6199614a456690103e58ec1949a058e6c49993c2
adan <adan69747@gmail.com>
2023-12-08 11:57:34 +0900

    增加配置服务ConfigService

--- lib/global.dart
+++ lib/global.dart
@@ -1,11 +1,13 @@
 
 import 'package:get/get.dart';
+import 'package:kkguoji/services/config_service.dart';
 import 'package:kkguoji/services/user_service.dart';
 import 'package:kkguoji/services/http_service.dart';
 
 class Global {
   static Future<void> init() async {
 
+    Get.put<ConfigService>(ConfigService());
     Get.put<UserService>(UserService());
     Get.put<HttpService>(HttpService());
   }
