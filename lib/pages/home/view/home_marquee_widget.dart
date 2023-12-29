ad6ed0a625e0e86ad58dbdb3505f1044e7184cda
gogoquan <675842100@qq.com>
2023-12-29 16:58:01 +0800

    解决冲突

--- lib/pages/home/view/home_marquee_widget.dart
+++ lib/pages/home/view/home_marquee_widget.dart
@@ -1,10 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:html2md/html2md.dart' as html2md;
 import 'package:marquee/marquee.dart';
 
 class KKHomeMarqueeWidget extends StatelessWidget {
-
   final String marqueeContent;
   const KKHomeMarqueeWidget(this.marqueeContent, {super.key});
 
@@ -16,20 +14,31 @@ class KKHomeMarqueeWidget extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Image.asset("assets/images/home_marquee_icon.png", width: 14, height: 14,),
-          const SizedBox(width: 10,),
-          if(marqueeContent.isNotEmpty)
-            Expanded(child: Marquee(
+          Image.asset(
+            "assets/images/home_marquee_icon.png",
+            width: 14,
+            height: 14,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          if (marqueeContent.isNotEmpty)
+            Expanded(
+                child: Marquee(
               text: html2md.convert(marqueeContent),
-              style: const TextStyle(color: Color.fromRGBO(255, 255, 255, 0.8),fontSize: 12),
-              scrollAxis: Axis.horizontal, crossAxisAlignment: CrossAxisAlignment.center,
-              blankSpace: 10.0, velocity: 80.0,
-              pauseAfterRound: const Duration(seconds: 1), startPadding: 10.0,accelerationDuration: const Duration(seconds: 1),
+              style: const TextStyle(
+                  color: Color.fromRGBO(255, 255, 255, 0.8), fontSize: 12),
+              scrollAxis: Axis.horizontal,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              blankSpace: 10.0,
+              velocity: 80.0,
+              pauseAfterRound: const Duration(seconds: 1),
+              startPadding: 10.0,
+              accelerationDuration: const Duration(seconds: 1),
               accelerationCurve: Curves.linear,
               decelerationDuration: const Duration(milliseconds: 500),
-              decelerationCurve: Curves.easeOut,))
-
-
+              decelerationCurve: Curves.easeOut,
+            ))
         ],
       ),
     );
