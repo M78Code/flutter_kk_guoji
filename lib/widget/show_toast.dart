053329fd19a5578778f8d890a7a3bf0d7e6e3372
adan <adan69747@gmail.com>
2024-01-19 18:26:28 +0900

    toast

--- lib/widget/show_toast.dart
+++ lib/widget/show_toast.dart
@@ -4,13 +4,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 class ShowToast {
   static showToast(String string) {
     if (string == null || string.length == 0) { return; }
-    Fluttertoast.showToast(
-        msg: string,
-        toastLength: Toast.LENGTH_SHORT,
-        timeInSecForIosWeb: 1,
-        gravity: ToastGravity.CENTER,
-        backgroundColor: const Color.fromRGBO(108, 122, 143, 0.8),
-        textColor: Colors.white,
-        fontSize: 15.0);
+    showToast(string);
   }
 }
