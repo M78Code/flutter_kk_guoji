78345986631e0cc7a8eb447e74954b99a7ef3fe4
Steve <>
2024-01-20 09:23:29 +0800

    bug修改

--- lib/widget/show_toast.dart
+++ lib/widget/show_toast.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:oktoast/oktoast.dart' as OkToast ;
 
 class ShowToast {
   static showToast(String string) {
     if (string == null || string.length == 0) { return; }
-    showToast(string);
+    OkToast.showToast(string);
   }
 }
