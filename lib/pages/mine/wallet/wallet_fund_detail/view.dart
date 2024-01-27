21ac503b42b0781cf87dda83ef30e63787e225d4
Lind <leonm78coder@gmail.com>
2024-01-27 16:27:16 +0900

    修复进入我的账号中的bug

--- lib/pages/mine/wallet/wallet_fund_detail/view.dart
+++ lib/pages/mine/wallet/wallet_fund_detail/view.dart
@@ -28,22 +28,20 @@ class _WalletFundDetailPageState extends State<WalletFundDetailPage> {
   Widget build(BuildContext context) {
     return _buildView();
   }
+
   Widget _buildView() {
     return Scaffold(
       appBar: AppBar(
         title: Text('资产明细'),
         centerTitle: true,
-        leading:  IconButton(
-          icon: Image.asset(
-              Assets.systemIconBack,
-              width: 20.w,
-              height: 20.w),
+        leading: IconButton(
+          icon: Image.asset(Assets.systemIconBack, width: 20.w, height: 20.w),
           onPressed: () {
             Get.back();
           },
         ),
       ),
-      body:  GetBuilder<WalletFundDetailLogic>(
+      body: GetBuilder<WalletFundDetailLogic>(
         id: 'WalletFundDetailPage',
         builder: (controller) {
           return Container(
@@ -60,41 +58,47 @@ class _WalletFundDetailPageState extends State<WalletFundDetailPage> {
                 key: UniqueKey(),
                 slivers: [
                   SliverPersistentHeader(
-                  pinned: true,
-                  delegate: CustomSliverHeaderDelegate(
-                      MineWalletBalanceWidget.kHeight + 20.w + RechargeSection.kHeight + 20.w + DateSelectionSection.kHeight + 15.w + (controller.userMoneyDetailsSearchList.isNotEmpty ? TransactionHeaderRow.kHeight : 0),
-                      child: ListView(
-                        children: [
-                          MineWalletBalanceWidget(),
-                          SizedBox(height: 20.w),
-                          RechargeSection(),
-                          SizedBox(height: 20.w),
-                          GetBuilder<WalletFundDetailLogic>(
-                            id: 'dateSelector',
-                            builder: (controller) {
-                              String? dateRange;
-                              if (controller.startDate != null && controller.endDate != null) {
-                                var startText = DateFormat('MM/dd').format(controller.startDate!);
-                                var endText = DateFormat('MM/dd').format(controller.endDate!);
-                                dateRange = startText + " - " + endText;
-                              }
-                              return DateSelectionSection(
-                                  dateTypes: controller.dateTypes,
-                                  selectType: controller.dateType ?? "",
-                                  selectDateRange: dateRange,
-                                  onTap: (selectType){
-                                    controller.onTapSwitchDate(selectType);
-                                  },
-                                  onTapSelectTime: (){
-                                    _showTimeWidget();
-                                  });
-                            },
-                          ),
-                          SizedBox(height: 15.w),
-                          if(controller.userMoneyDetailsSearchList.isNotEmpty)  TransactionHeaderRow(),
-                        ]
-                      )
-                  )),
+                      pinned: true,
+                      delegate: CustomSliverHeaderDelegate(
+                          MineWalletBalanceWidget.kHeight +
+                              20.w +
+                              RechargeSection.kHeight +
+                              20.w +
+                              DateSelectionSection.kHeight +
+                              15.w +
+                              (controller.userMoneyDetailsSearchList.isNotEmpty
+                                  ? TransactionHeaderRow.kHeight
+                                  : 0),
+                          child: ListView(children: [
+                            MineWalletBalanceWidget(),
+                            SizedBox(height: 20.w),
+                            RechargeSection(),
+                            SizedBox(height: 20.w),
+                            GetBuilder<WalletFundDetailLogic>(
+                              id: 'dateSelector',
+                              builder: (controller) {
+                                String? dateRange;
+                                if (controller.startDate != null && controller.endDate != null) {
+                                  var startText = DateFormat('MM/dd').format(controller.startDate!);
+                                  var endText = DateFormat('MM/dd').format(controller.endDate!);
+                                  dateRange = startText + " - " + endText;
+                                }
+                                return DateSelectionSection(
+                                    dateTypes: controller.dateTypes,
+                                    selectType: controller.dateType ?? "",
+                                    selectDateRange: dateRange,
+                                    onTap: (selectType) {
+                                      controller.onTapSwitchDate(selectType);
+                                    },
+                                    onTapSelectTime: () {
+                                      _showTimeWidget();
+                                    });
+                              },
+                            ),
+                            SizedBox(height: 15.w),
+                            if (controller.userMoneyDetailsSearchList.isNotEmpty)
+                              TransactionHeaderRow(),
+                          ]))),
                   TransactionListSection(),
                 ],
               ),
@@ -104,6 +108,7 @@ class _WalletFundDetailPageState extends State<WalletFundDetailPage> {
       ).safeArea(),
     );
   }
+
   void _showTimeWidget() {
     Widget child = CustomDatePicker(
       startDate: controller.startDate ?? DateTime.now(),
@@ -122,29 +127,28 @@ class _WalletFundDetailPageState extends State<WalletFundDetailPage> {
     showOverlay(child, buttonPosition.dx, buttonPosition.dy + buttonSize.height);
   }
 
-  void showOverlay(Widget child, double left,double top) {
+  void showOverlay(Widget child, double left, double top) {
     overlayEntry = OverlayEntry(
-      builder: (context) =>
-          Stack(
-            children: [
-              Positioned.fill(
-                child: GestureDetector(
-                  onTap: () {
-                    // Remove the overlay when tapped outside the image
-                    overlayEntry.remove();
-                  },
-                  child: Container(
-                    color: Colors.transparent,
-                  ),
-                ),
+      builder: (context) => Stack(
+        children: [
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: () {
+                // Remove the overlay when tapped outside the image
+                overlayEntry.remove();
+              },
+              child: Container(
+                color: Colors.transparent,
               ),
-              Positioned(
-                left: left,
-                top: top,
-                child: child,
-              ),
-            ],
+            ),
           ),
+          Positioned(
+            left: left,
+            top: top,
+            child: child,
+          ),
+        ],
+      ),
     );
     Overlay.of(context)?.insert(overlayEntry);
   }
@@ -154,7 +158,7 @@ class CustomSliverHeaderDelegate extends SliverPersistentHeaderDelegate {
   final Widget child;
   final double maxHeight;
 
-  CustomSliverHeaderDelegate(this.maxHeight,  {required this.child});
+  CustomSliverHeaderDelegate(this.maxHeight, {required this.child});
 
   @override
   Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
---
+++ lib/pages/mine/wallet/wallet_record/widgets/charge_record_page.dart
@@ -0,0 +1,124 @@
+import 'package:easy_refresh/easy_refresh.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:kkguoji/common/extension/ex_widget.dart';
+import 'package:kkguoji/generated/assets.dart';
+import 'package:kkguoji/pages/mine/wallet/wallet_record/widgets/record_date_selectiong_widget.dart';
+import 'package:kkguoji/pages/mine/wallet/wallet_record/widgets/wallet_record_balance_widget.dart';
+import 'package:kkguoji/pages/mine/wallet/wallet_record/widgets/wallet_record_list.dart';
+
+import '../../../bet_list/widgets/custom_date_picker.dart';
+import '../../../bet_list/widgets/date_selection_section.dart';
+import '../logic.dart';
+import 'package:intl/intl.dart';
+
+class ChargeRecordPage extends StatelessWidget {
+  ChargeRecordPage({Key? key}) : super(key: key);
+  final controller = Get.put(WalletRecordLogic());
+  late OverlayEntry overlayEntry;
+
+  @override
+  Widget build(BuildContext context) {
+    return  GetBuilder<WalletRecordLogic>(
+      id: "chargePage",
+      builder: (_) {
+        return _buildView(context);
+      },
+    );
+  }
+  Widget _buildView(BuildContext context) {
+    return Scaffold(
+      body:  Container(
+          padding: EdgeInsets.symmetric(horizontal: 12.w),
+          child:EasyRefresh(
+            controller: controller.userRechargeState.refreshController,
+            onRefresh: () async {
+              controller.onRefresh();
+            },
+            onLoad: () async {
+              controller.onLoading();
+            },
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(child: SizedBox(height: 10.w)),
+                SliverToBoxAdapter(child:  WalletRecordBalanceWidget()),
+                SliverToBoxAdapter(child: SizedBox(height: 20.w)),
+                SliverToBoxAdapter(child: GetBuilder<WalletRecordLogic>(
+                  id: 'rechargeDateSelector',
+                  builder: (controller) {
+                    String? dateRange;
+                    if (controller.startDate != null && controller.endDate != null) {
+                      var startText = DateFormat('MM/dd').format(controller.startDate!);
+                      var endText = DateFormat('MM/dd').format(controller.endDate!);
+                      dateRange = startText + " - " + endText;
+                    }
+                    return DateSelectionSection(
+                        dateTypes: controller.userRechargeState.dateTypes,
+                        selectType: controller.dateType ?? "",
+                        selectDateRange: dateRange,
+                        onTap: (selectType){
+                          controller.onTapSwitchDate(selectType);
+                        },
+                        onTapSelectTime: (){
+                          _showTimeWidget(context);
+                        });
+                  },
+                )),
+                SliverToBoxAdapter(child: SizedBox(height: 15.w)),
+                if (controller.userRechargeState.userRechargeModels.isEmpty) SliverToBoxAdapter(child: Container(alignment: Alignment.topCenter,child: Image.asset("assets/images/rebate/nodata.png", width: 200.w, height: 223.w,))),
+                if (controller.userRechargeState.userRechargeModels.isEmpty == false) SliverFillRemaining(child: WalletRecordList(isWithDrawRecord: false))
+              ],
+            ),
+          )
+      ).safeArea(),
+    );
+  }
+  void _showTimeWidget(BuildContext context) {
+    var startDate = controller.startDate;
+    var endDate = controller.endDate;
+
+    Widget child = CustomDatePicker(
+      startDate: startDate ?? DateTime.now(),
+      endDate: endDate ?? DateTime.now(),
+      cancelAction: () {
+        overlayEntry.remove();
+      },
+      dateConfirmAction: (startDate, endDate) {
+        controller.switchDate(startDate, endDate);
+        overlayEntry.remove();
+      },
+    );
+    RenderBox renderBox = dateSelectionSectionKey.currentContext!.findRenderObject() as RenderBox;
+    final buttonSize = renderBox.size;
+    final buttonPosition = renderBox.localToGlobal(Offset.zero);
+    showOverlay(child, buttonPosition.dx, buttonPosition.dy + buttonSize.height, context);
+  }
+
+  void showOverlay(Widget child, double left,double top, BuildContext context) {
+    overlayEntry = OverlayEntry(
+      builder: (context) =>
+          Stack(
+            children: [
+              Positioned.fill(
+                child: GestureDetector(
+                  onTap: () {
+                    // Remove the overlay when tapped outside the image
+                    overlayEntry.remove();
+                  },
+                  child: Container(
+                    color: Colors.transparent,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: left,
+                top: top,
+                child: child,
+              ),
+            ],
+          ),
+    );
+    Overlay.of(context)?.insert(overlayEntry);
+  }
+}
